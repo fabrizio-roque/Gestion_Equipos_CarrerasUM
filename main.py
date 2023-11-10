2ff8e0cd1e9cbdbbc9a6ca044570c907a401b358
AgusR <aroque@correo.um.edu.uy>
2023-11-10 15:52:56 -0300

    More changes, Prepared test setup for demo purposes and Added full race simulation with incident handling and score calculations.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from datetime import datetime
-import random # Solo para simular la carrera
+import time
+import sys
+import random 
 
 class Empleado:
     FORMATO_FECHA = "%d-%m-%Y"
@@ -32,6 +34,9 @@ class Piloto(Empleado):
         self._numero_auto = numero_auto
         self._puntaje_campeonato = puntaje_campeonato
         self._lesionado = lesionado
+        
+    def actualizar_puntaje_campeonato(self, puntos):
+        self._puntaje_campeonato += puntos
 
 
 class Mecanico(Empleado):
@@ -128,7 +133,7 @@ import random
 def generar_imprevistos_aleatorios():
     imprevisto = Imprevisto()
     
-    # Supongamos que hay un 10% de probabilidad de que un piloto se lesione
+    #10% de probabilidad de que un piloto se lesione
     if random.random() < 0.10:
         imprevisto.lesionado = True
         
@@ -144,6 +149,19 @@ def generar_imprevistos_aleatorios():
     
     return imprevisto
 
+def print_loading(duration=5):
+    end_time = time.time() + duration
+    load_chars = ['|', '/', '-', '\\']
+    i = 0
+
+    while time.time() < end_time:
+        sys.stdout.write('\r' + load_chars[i % len(load_chars)] + " Simulando...")
+        sys.stdout.flush()
+        time.sleep(0.1)
+        i += 1
+
+    sys.stdout.write('\rCarrera Terminada!!        \n')
+    
 def simular_carrera(autos, equipos):
     pilotos = []
     for equipo in equipos:
@@ -169,34 +187,32 @@ def simular_carrera(autos, equipos):
         imprevisto = generar_imprevistos_aleatorios()
         imprevistos_pilotos[piloto._id] = imprevisto
 
-    print("\nSimulación...\n")
+    print_loading()
     resultados = []
+    
+    
 
     for piloto in pilotos:
         auto = next((auto for auto in autos if auto._modelo == piloto._numero_auto), None)
         if auto:
             imprevisto = imprevistos_pilotos[piloto._id]
-            
+        
             if imprevisto.abandono:
                 score_final = 0
             else:
-                # Suponiendo que la suma_score_mecanicos es un valor constante
-                # si necesitas calcularlo, deberás agregar la lógica correspondiente
                 suma_score_mecanicos = 10  # Valor de ejemplo
                 score_final = suma_score_mecanicos + auto._score + piloto._score - 5 * imprevisto.errores_en_pits - 8 * imprevisto.penalidades
 
-            resultados.append((piloto._nombre, score_final))
+            resultados.append((piloto, score_final))
 
     resultados.sort(key=lambda x: x[1], reverse=True)  # Ordena los resultados de mayor a menor score
 
     print("Resultados de la carrera:")
     puntos = [25, 18, 15, 12, 10, 8, 6, 4, 2, 1]
-    for i, (nombre, score) in enumerate(resultados, 1):
-        if i <= len(puntos):
-            print(f"{i}. {nombre} - Score: {score:.2f} - Puntos obtenidos: {puntos[i-1]}")
-        else:
-            print(f"{i}. {nombre} - Score: {score:.2f}")
-
+    for i, (piloto, score) in enumerate(resultados, 1):
+        puntos_adicionales = puntos[i-1] if i <= len(puntos) else 0
+        piloto.actualizar_puntaje_campeonato(puntos_adicionales)
+        print(f"{i}. {piloto._nombre} - Score: {score:.2f} - Puntos obtenidos: {puntos_adicionales}")
 def realizar_consultas(equipos):
     while True:
         print("\nSeleccione una opción de consulta:")
@@ -244,7 +260,122 @@ def realizar_consultas(equipos):
 
         elif opcion == 6:
             break
+def configurar_prueba_y_simular(equipos, autos):
+    # Limpiamos los datos existentes para la prueba
+    equipos.clear()
+    autos.clear()
 
+    # Creando autos de prueba
+    autos.append(Auto("Ferrari", 90, "Rojo"))
+    autos.append(Auto("Lamborghini", 85, "Azul"))
+    autos.append(Auto("Nissan", 88, "Verde"))
+    autos.append(Auto("Renault", 92, "Amarillo"))
+    autos.append(Auto("Redbull", 87, "Blanco"))
+    autos.append(Auto("BMW", 91, "Violeta"))
+    autos.append(Auto("BYD", 95, "Celeste"))
+    autos.append(Auto("Audi", 80, "Naranja"))
+    autos.append(Auto("Chevrolet", 80, "Naranja"))
+    autos.append(Auto("Alfa Romeo", 80, "Naranja"))
+
+    # Creando equipos y empleados de prueba
+    equipo1 = Equipo("RedBull", "País1", 2000)
+    equipo2 = Equipo("Mercedes", "País2", 2001)
+    equipo3 = Equipo("Alpine", "País3", 1999)
+    equipo4 = Equipo("Aston Martin", "País4", 2002)
+    equipo5 = Equipo("Haas", "País5", 2003)
+    equipo6 = Equipo("Ferrari", "País6", 2004)
+    equipo7 = Equipo("Peru", "País7", 2005)
+    equipo8 = Equipo("Argentina", "País8", 2006)
+    equipo9 = Equipo("Uruguay", "País9", 2007)
+    equipo10 = Equipo("Brasil", "País10", 2008)
+    
+    
+    # Pilotos
+    piloto1 = Piloto(1, "Agustin", 30, "Italiana", "01-01-2002", 50000, 95, "Ferrari", 10, False)
+    piloto2 = Piloto(2, "German", 32, "Uruguaya", "02-02-1988", 60000, 92, "Lamborghini", 8, False)
+    piloto3 = Piloto(3, "Arturo", 28, "Brasilera", "03-03-1992", 55000, 90, "Nissan", 6, False)
+    piloto4 = Piloto(4, "Juan", 35, "Argentina", "04-04-1985", 65000, 88, "Renault", 4, False)
+    piloto5 = Piloto(5, "Axel", 33, "Uruguaya", "05-05-1987", 70000, 85, "Audi", 2, False)
+    piloto6 = Piloto(6, "Matias", 31, "Argentina", "06-06-1989", 75000, 82, "Redbull", 0, False)
+    piloto7 = Piloto(7, "Joaquin", 29, "Peruana", "07-07-1991", 80000, 80, "BMW", 0, False)
+    piloto8 = Piloto(8, "Marcos", 27, "Peruana", "08-08-1993", 85000, 78, "BYD", 0, False)
+    piloto9 = Piloto(9, "Sebastian", 25, "Boaliviana", "09-09-1995", 90000, 75, "Chevrolet", 0, False)
+    piloto10 = Piloto(10, "Alberto", 23, "Mexicana", "09-09-1995", 95000, 72, "Alfa Romeo", 0, False)
+
+    # Mecánicos
+    mecanico1 = Mecanico(4, "Mecanico1", 40, "Francesa", "04-04-1980", 40000, 80)
+    mecanico2 = Mecanico(5, "Mecanico2", 38, "Francesa", "05-05-1982", 42000, 85)
+    mecanico3 = Mecanico(6, "Mecanico3", 36, "Francesa", "06-06-1984", 44000, 90)
+    mecanico4 = Mecanico(7, "Mecanico4", 34, "Francesa", "07-07-1986", 46000, 95)
+    mecanico5 = Mecanico(8, "Mecanico5", 32, "Francesa", "08-08-1988", 48000, 100)
+    mecanico6 = Mecanico(9, "Mecanico6", 30, "Francesa", "09-09-1990", 50000, 105)
+    mecanico7 = Mecanico(10, "Mecanico7", 28, "Francesa", "10-10-1992", 52000, 110)
+    mecanico8 = Mecanico(11, "Mecanico8", 26, "Francesa", "11-11-1994", 54000, 115)
+    mecanico9 = Mecanico(12, "Mecanico9", 24, "Francesa", "12-12-1996", 56000, 120)
+    mecanico10 = Mecanico(13, "Mecanico10", 22, "Francesa", "09-09-1995", 58000, 125)
+
+    # Directores de equipo
+    director1 = DirectorEquipo(6, "Director1", 50, "Alemana", "06-06-1970", 100000)
+    director2 = DirectorEquipo(7, "Director2", 48, "Alemana", "07-07-1972", 95000)
+    director3 = DirectorEquipo(8, "Director3", 46, "Alemana", "08-08-1974", 90000)
+    director4 = DirectorEquipo(9, "Director4", 44, "Alemana", "09-09-1976", 85000)
+    director5 = DirectorEquipo(10, "Director5", 42, "Alemana", "10-10-1978", 80000)
+    director6 = DirectorEquipo(11, "Director6", 40, "Alemana", "11-11-1980", 75000)
+    director7 = DirectorEquipo(12, "Director7", 38, "Alemana", "12-12-1982", 70000)
+    director8 = DirectorEquipo(13, "Director8", 36, "Alemana", "09-09-1995", 65000)
+    director9 = DirectorEquipo(14, "Director9", 34, "Alemana", "09-09-1995", 60000)
+    director10 = DirectorEquipo(15, "Director10", 32, "Alemana", "09-09-1995", 55000)
+    
+    # Limpiamos los puntajes de campeonato de los pilotos
+    for piloto in [piloto1, piloto2, piloto3, piloto4, piloto5, piloto6, piloto7, piloto8, piloto9, piloto10]:
+        piloto._puntaje_campeonato = 0
+    
+
+    equipo1.agregar_empleado(piloto1)
+    equipo1.agregar_empleado(mecanico1)
+    equipo1.agregar_empleado(director1)
+   
+    equipo2.agregar_empleado(piloto2)
+    equipo2.agregar_empleado(mecanico2)
+    equipo2.agregar_empleado(director2)
+    
+    equipo3.agregar_empleado(piloto3)
+    equipo3.agregar_empleado(mecanico3)
+    equipo3.agregar_empleado(director3)
+    
+    equipo4.agregar_empleado(piloto4)
+    equipo4.agregar_empleado(mecanico4)
+    equipo4.agregar_empleado(director4)
+    
+    equipo5.agregar_empleado(piloto5)
+    equipo5.agregar_empleado(mecanico5)
+    equipo5.agregar_empleado(director5)
+    
+    equipo6.agregar_empleado(piloto6)
+    equipo6.agregar_empleado(mecanico6)
+    equipo6.agregar_empleado(director6)
+    
+    equipo7.agregar_empleado(piloto7)
+    equipo7.agregar_empleado(mecanico7)
+    equipo7.agregar_empleado(director7)
+    
+    equipo8.agregar_empleado(piloto8)
+    equipo8.agregar_empleado(mecanico8)
+    equipo8.agregar_empleado(director8)
+    
+    equipo9.agregar_empleado(piloto9)
+    equipo9.agregar_empleado(mecanico9)
+    equipo9.agregar_empleado(director9)
+    
+    equipo10.agregar_empleado(piloto10)
+    equipo10.agregar_empleado(mecanico10)
+    equipo10.agregar_empleado(director10)
+
+    equipos.extend([equipo1, equipo2, equipo3, equipo4, equipo5, equipo6, equipo7, equipo8, equipo9, equipo10])
+
+    #simulamos la carrera con datos anteriores
+    simular_carrera(autos, equipos)
+    
 def main():
     equipos = []
     autos = []
@@ -256,8 +387,9 @@ def main():
         print("4. Simular carrera")
         print("5. Realizar consultas")
         print("6. Finalizar programa")
+        print("7. Configurar prueba y simular")
 
-        opcion = valida_opcion(1, 6)
+        opcion = valida_opcion(1, 7)
 
         if opcion == 1:
             while True:
@@ -331,6 +463,8 @@ def main():
             realizar_consultas(equipos)
         elif opcion == 6:
             break
+        elif opcion == 7:
+            configurar_prueba_y_simular(equipos, autos)
 
 
 main()
